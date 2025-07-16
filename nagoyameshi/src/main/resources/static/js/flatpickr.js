61882a5cf97f1a1ea69e603e287c2918c60f2356
dh291422 <dh291422@gmail.com>
2025-07-16 15:46:40 +0900

    Delete nagoyameshi directory

--- nagoyameshi/src/main/resources/static/js/flatpickr.js
+++
@@ -1,9 +0,0 @@
-let maxDate = new Date();
-maxDate = maxDate.setMonth(maxDate.getMonth() + 3);
-
-flatpickr('#fromCheckinDate', {
- mode: "single",
- locale: 'ja',
- minDate: 'today',
- maxDate: maxDate
-});
